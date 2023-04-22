84a68b8a2434dba23ef3428652d8855865bf2bc7
Code Sandbox <hello@codesandbox.io>
2023-04-22 04:51:48 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.7046a5db781af186c4dc0c322c1cc445.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1ca205a660412bc348f55adb54e36a2",
+    "url": "https://jagadeeshreddiie.github.io/csb-t7nmed/index.html"
+  },
+  {
+    "revision": "199f40ad9b6c9c65e54b",
+    "url": "https://jagadeeshreddiie.github.io/csb-t7nmed/static/js/2.4c9de9a1.chunk.js"
+  },
+  {
+    "revision": "99ea8ad0135a10593fc5",
+    "url": "https://jagadeeshreddiie.github.io/csb-t7nmed/static/js/main.3bff4834.chunk.js"
+  },
+  {
+    "revision": "2876e74a97f735911e6b",
+    "url": "https://jagadeeshreddiie.github.io/csb-t7nmed/static/js/runtime-main.0b10b244.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "https://jagadeeshreddiie.github.io/csb-t7nmed/precache-manifest.7046a5db781af186c4dc0c322c1cc445.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://jagadeeshreddiie.github.io/csb-t7nmed/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
